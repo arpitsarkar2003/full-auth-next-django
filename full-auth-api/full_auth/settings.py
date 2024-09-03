e4bbb14c0296be38eee98cdad246fd259e2bdb68
Arpit From Laptop <arpitsarkar16@gmail.com>
2024-09-03 14:15:12 +0530

    Custom User Model Created

--- full-auth-api/full_auth/settings.py
+++ full-auth-api/full_auth/settings.py
@@ -154,3 +154,6 @@ Djoser = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'users.UserAccount'
